f117130756e3e3166934c88cc61aecb601d2505e
vasu <vasumadhavarapu@gmail.com>
2022-08-15 02:52:34 +0000

    assignment

--- src/components/ResourceDetailComponent/sortMenu.js
+++ src/components/ResourceDetailComponent/sortMenu.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
